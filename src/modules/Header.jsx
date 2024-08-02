89119e4112f17f3dad4b7f038edea4f4ec1d8cb9
IrekYkubov <irekyyakubov@gmail.com>
2024-08-02 18:54:52 +0400

    Роутинг и создание корзины

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
+
 export const Header = () => {
   return (
     <header className="header">
         <div className="container header__container">
-          <a className="header__logo-link">
+          <Link to="/" className="header__logo-link">
             <img className="header__logo" src="image/logo.svg" alt="Логотип Cup Time" />
-          </a>
+          </Link>
           <nav className="header__nav">
             <ul className="header__menu">
               <li className="header__menu-item">
@@ -34,7 +36,7 @@ export const Header = () => {
               </li>
             </ul>
           </nav>
-          <a href="cart.html" className="header__cart-link">6</a>
+          <Link to="cart" className="header__cart-link">6</Link>
         </div>
       </header>
   )
