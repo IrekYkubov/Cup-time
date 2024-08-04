1e1d4f7b3ccd54e50eb5eadba5f2cb288821a65b
IrekYkubov <irekyyakubov@gmail.com>
2024-08-04 21:05:14 +0400

    Доработка модалки и адаптива

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,9 +1,21 @@
 import { Link, useLocation } from "react-router-dom"
 import { useCart } from "../context/CartContext";
+import { useProducts } from "../context/ProductContext";
+import { useState } from "react";
 
 export const Header = () => {
+  const {categories} = useProducts();
   const {cart} = useCart();
   const location = useLocation();
+
+  const [isMenuOpen, setIsOpenMenu] = useState(false);
+  const closeMenu = () => {
+    setIsOpenMenu(false);
+  }
+  const openMenu = () => {
+    setIsOpenMenu(true);
+  }
+
   const getActiveClass = (category) => {
     const currentCategory = new URLSearchParams(location.search).get('category');
     return currentCategory === category ? 'active' : '';
@@ -14,36 +26,34 @@ export const Header = () => {
           <Link to="/" className="header__logo-link">
             <img className="header__logo" src="image/logo.svg" alt="Логотип Cup Time" />
           </Link>
-          <nav className="header__nav">
+          <nav className={`header__nav ${isMenuOpen ? 'active' : ''}`}>
             <ul className="header__menu">
-              <li className="header__menu-item">
-                <Link to="/products?category=tea" className={`header__menu-link ${getActiveClass('tea')}`}>
-                  Чай
-                </Link>
+              {Object.entries(categories).map(([key, value]) => (
+                <li key={key} className="header__menu-item">
+                  <Link to={`/products?category=${key}`} className={`header__menu-link ${getActiveClass(key)}`} onClick={closeMenu}>
+                    {value}
+                  </Link>
               </li>
-              <li className="header__menu-item">
-                <Link to="/products?category=coffee" className={`header__menu-link ${getActiveClass('coffee')}`}>
-                  Кофе
-                </Link>
-              </li>
-              <li className="header__menu-item">
-                <Link to="/products?category=teapots" className={`header__menu-link ${getActiveClass('teapots')}`}>
-                  Чайники
-                </Link>
-              </li>
-              <li className="header__menu-item">
-                <Link to="/products?category=cezves" className={`header__menu-link ${getActiveClass('cezves')}`}>
-                  Турки
-                </Link>
-              </li>
-              <li className="header__menu-item">
-                <Link to="/products?category=other" className={`header__menu-link ${getActiveClass('other')}`}>
-                  Прочее
-                </Link>
-              </li>
+              ))}
             </ul>
+            <button className="header__close-btn" onClick={closeMenu}>
+              <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="7.28174" y="7.07532" width="20" height="1" transform="rotate(45 7.28174 7.07532)" fill="#D9D9D9" />
+                <rect x="6.5752" y="21.2173" width="20" height="1" transform="rotate(-45 6.5752 21.2173)" fill="#D9D9D9" />
+              </svg>
+            </button>
           </nav>
-          <Link to="cart" className="header__cart-link">{cart ? cart.length : ''}</Link>
+          <div className="header__control">
+            <Link to="cart" className="header__cart-link">{cart ? cart.length : ''}</Link>
+            <button className="header__burger" onClick={openMenu}>
+              <svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="4" y="9.5" width="20" height="1" fill="#D9D9D9" />
+                <rect x="4" y="14.5" width="20" height="1" fill="#D9D9D9" />
+                <rect x="4" y="19.5" width="20" height="1" fill="#D9D9D9" />
+              </svg>
+            </button>
+          </div>
+          
         </div>
       </header>
   )
--- src/modules/ProductModal.jsx
+++ src/modules/ProductModal.jsx
@@ -2,6 +2,7 @@ import Modal from "react-modal";
 import { API_URL } from "../const";
 import { useState } from "react";
 import { useCart } from "../context/CartContext";
+import s from "./ProductModal.module.css"
 
 function isSmallScreen() {
   if (typeof window !== 'undefined') {
@@ -10,21 +11,7 @@ function isSmallScreen() {
   return false;
 }
 
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    padding: "0 10px",
-    maxWidth: isSmallScreen() ? "290px" : "740px",
-    width: "100%",
-    border: "none",
-    backgroundColor: "transparent",
-  },
-}
+
 
 Modal.setAppElement('#root');
 
@@ -51,32 +38,53 @@ export const ProductModal = ({isOpen, onRequestClose, data}) => {
 
   return (
     <Modal
+    className={s.modal}
+    overlayClassName={s.overlay}
     isOpen={isOpen}
     onRequestClose={onRequestClose}
-    style={customStyles}
-    contentLabel = "Product Modal"
+    
+    contentLabel = {data.title}
     >
       <div className="modal-wrapper">
-        <img className="modal__image" src={`${API_URL}${data.img}`} alt={data.title} />
-        <div className="modal-description">
-          <h2 className="modal__title">{data.title}</h2>
-          <p className="modal__price">{data.price}</p>
-          <ul>
+        <img className={s.image} src={`${API_URL}${data.img}`} alt={data.title} />
+        <div className={s.content}>
+          <div className={s.header}>
+            <h2 className={s.title}>{data.title}</h2>
+            <p className={s.price}>{data.price}&nbsp;₽</p>
+          </div>
+          <ul className={s.list}>
             {Object.entries(data.additional).map(([key, value]) => (
-              <li className="modal__properties-item" key={key}>
-                <span className="modal__properties-product">{key}</span> <span className="modal__properties-value">{value}</span>
+              <li className={s.item} key={key}>
+                <span className={s.field}>{key}</span> <span className={s.value}>{value}</span>
               </li>
             ))}
           </ul>
-          <div>
-            <button onClick={handleDecrease}>-</button>
-            <input type="number" value={quantity} readOnly />
-            <button onClick={handleIncrease}>+</button>
+          <div className={s.footer}>
+            <div className={s.count}>
+              <button className={s.btn} onClick={handleDecrease}>
+                <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="0.5" y="0.5" width="35" height="35" rx="3.5" stroke="#B8B8B8"/>
+                  <rect x="12" y="17" width="12" height="2" fill="#1D1C1D"/>
+                </svg>
+              </button>
+              <input className={s.number} type="number" value={quantity} readOnly />
+              <button className={s.btn} onClick={handleIncrease}>
+                <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="0.5" y="0.5" width="35" height="35" rx="3.5" stroke="#B8B8B8"/>
+                  <rect x="12" y="17.25" width="12" height="1.5" fill="#1D1C1D"/>
+                  <rect x="17.25" y="24" width="12" height="1.5" transform="rotate(-90 17.25 24)" fill="#1D1C1D"/>
+                </svg>
+              </button>
+            </div>
+            <button className={s.btnAddCart} onClick={handleAddToCart}>Добавить</button>
           </div>
-
-
-          <button className="modal__button-add" onClick={handleAddToCart}>Добваить в корзину</button>
-          <button className="modal__button-close" onClick={onRequestClose}>&#215;</button>
+          
+          <button className={s.btnCloseCard} onClick={onRequestClose}>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="5.71228" y="14.1975" width="12" height="1.5" transform="rotate(-45 5.71228 14.1975)" fill="#B8B8B8" />
+              <rect x="14.1976" y="15.2582" width="12" height="1.5" transform="rotate(-135 14.1976 15.2582)" fill="#B8B8B8" />
+            </svg>
+          </button>
         </div>
       </div>
     </Modal>
